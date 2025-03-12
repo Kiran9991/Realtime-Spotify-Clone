8b5090de06aa9ebe5599ae1a248fcb911f97f05a
Kiran9991 <kiranagiwale@gmail.com>
2025-03-12 13:50:21 +0530

    frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-red-500 text-5xl">Hello world</h1>
+      <div className="bg-red-500">ok</div>
+      <Button>click</Button>
+    </>
+  )
+}
+
+export default App
